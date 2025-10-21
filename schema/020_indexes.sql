ba09a9e054eedd5e95d585f950771905e632ad9e
Tolben <vitek.pasek@gmail.com>
2025-10-22 00:00:16 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: payments
 CREATE INDEX idx_payments_created_at ON payments (created_at);
--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: payments
 ALTER TABLE payments ADD CONSTRAINT fk_payments_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE SET NULL;
