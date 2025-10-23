9bb72c9e92c19ac2347557ee0e0216568f57398e
Tolben <vitek.pasek@gmail.com>
2025-10-23 12:23:32 +0200

    chore(codegen): add/update generated PHP (Module, Joins, Repository, Mapper, Exceptions) from schema-map

--- src/Service/PaymentsAggregateService.php
+++ src/Service/PaymentsAggregateService.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace BlackCat\Database\Packages\Payments\Service;
 
-use BlackCat\Core\Database\Database;
+use BlackCat\Core\Database;
 use BlackCat\Database\Packages\Payments\Dto\PaymentDto;
 use BlackCat\Database\Packages\Payments\Mapper\PaymentDtoMapper;
 use BlackCat\Database\Packages\Payments\Repository\PaymentRepository;
